3102185b9bc2506ba9d8241fd6b0076b8547a57d
Nick Petalas <webmasternikos@gmail.com>
2021-11-01 21:34:29 +0000

    css cleanup WIP

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,18 +1,8 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.com/docs/use-static-query/
- */
-
 import * as React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+import {useStaticQuery, graphql} from "gatsby"
 
-import Header from "./header"
-import "./layout.css"
-
-const Layout = ({ children }) => {
+const Layout = ({children}) => {
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
@@ -25,23 +15,10 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <footer
-          style={{
-            marginTop: `2rem`,
-          }}
-        >
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
+      <div class="min-h-screen min-w-max flex flex-col justify-between items-stretch bg-gray-500">
+        <main class="flex flex-col flex-1 max-w-lg self-center items-center outline-black m-2">{children}</main>
+        <footer class="bg-blue-500 p-2 mt-2 text-center">
+          © {new Date().getFullYear()}, Nick Petalas
         </footer>
       </div>
     </>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
+import {Link} from "gatsby"
+import {StaticImage} from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
@@ -8,23 +8,19 @@ import Seo from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p class="m-2 text-center bg-blue-400">Welcome to your new Gatsby site.</p>
-    
-    <p>Now go build something great.</p>
+    <p class="m-2 text-center bg-blue-500">Page 1</p>
+
     <StaticImage
+      class="m-1"
       src="../images/gatsby-astronaut.png"
       width={300}
       quality={95}
-      formats={["auto", "webp", "avif"]}
+      formats={["webp"]}
       alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
     />
+
     <p>
       <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link> <br />
-      <Link to="/using-ssr">Go to "Using SSR"</Link> <br />
-      <Link to="/using-dsg">Go to "Using DSG"</Link>
     </p>
   </Layout>
 )
