2d611ca4a5a85b8d7a13c9062a5fb6d5984514c7
Nick Petalas <webmasternikos@gmail.com>
2021-11-01 20:39:51 +0000

    setting up tailwind css

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,8 @@ const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
     <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <p class="m-2 text-center bg-blue-400">Welcome to your new Gatsby site.</p>
+    
     <p>Now go build something great.</p>
     <StaticImage
       src="../images/gatsby-astronaut.png"
