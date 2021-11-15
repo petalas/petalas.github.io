35d201f30464751045c71c0e432b3a196ba65f1c
Nick Petalas <webmasternikos@gmail.com>
2021-11-15 16:56:20 +0000

    extracted about section

---
+++ src/components/about.section.js
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+const About = (props) => {
+    return (
+        <section {...props} >
+            <h2 class="text-2xl text-primary-200">Hi, I'm Nick.</h2>
+            <h3 class="text-xl text-primary-300">Senior Software Engineer, based in London, originally from Greece.</h3>
+            <h4 class="text-lg text-primary-400">My interests include but are not limited to: science, technology, cryptocurrencies, personal finance, photography.</h4>
+        </section>
+    );
+}
+
+export default About;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import {StaticImage} from "gatsby-plugin-image"
 import * as React from "react"
+import About from "../components/about.section"
 import Experience from "../components/experience.section"
 import Layout from "../components/layout"
 import Links from "../components/links.section"
@@ -12,21 +13,16 @@ const IndexPage = () => (
     <Seo title="Nick Petalas Portfolio Homepage" meta="" />
     <div class="flex-1 text-primary-500 flex flex-col text-xs">
 
-      <section id="about" class="p-2 rounded-lg flex flex-row bg-secondary-900 border-primary-300 mb-2">
+      <section id="about" class="p-1 md:p-2 rounded-lg flex flex-grow bg-secondary-900 border-primary-300 mb-2">
         <StaticImage
           class="w-1/3"
           loading="eager"
           src="../images/nick.jpg"
-          width={256}
           quality={95}
-          alt="Profile Picture"
+          alt="Nick Petalas - Profile Picture"
         />
 
-        <section class="flex-grow p-2">
-          <h2 class="text-lg text-primary-300">Nick Petalas</h2>
-          <h3 class="text-base text-primary-400">Senior Software Engineer</h3>
-          <p class="text-sm text-primary-500">Science, technology, crypto, photography.</p>
-        </section>
+        <About class="w-2/3 p-1 md:p-2" />
       </section>
 
 
@@ -37,7 +33,7 @@ const IndexPage = () => (
       <Projects id="projects" />
 
       <h2 class="pt-4 text-lg text-secondary-200">Get in touch</h2>
-      <Links i="links" class="bg-secondary-600 p-2 rounded border-2 border-primary-400 text-secondary-300"/>
+      <Links i="links" class="bg-secondary-600 p-2 rounded border-2 border-primary-400 text-secondary-300" />
     </div>
 
   </Layout>
