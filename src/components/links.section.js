840f9f7f6d6f2d60a2f4eed4a7e0532f528faf2f
Nick Petalas <webmasternikos@gmail.com>
2021-11-20 09:01:45 +0000

    updated links section icons

--- src/components/links.section.js
+++ src/components/links.section.js
@@ -1,5 +1,9 @@
 import * as React from "react";
+import ArtgenIcon from "../icons/artgen";
+import EmailIcon from "../icons/email";
+import GithubIcon from "../icons/github";
 import HomeIcon from "../icons/home";
+import LinkedinIcon from "../icons/linkedin";
 
 const Links = (props) => {
     return (
@@ -8,19 +12,19 @@ const Links = (props) => {
             <p>Let me know if you have suggestions for improvements or if you'd like to collaborate on any of my side projects.</p>
             <div class="container flex flex-grow mx-auto justify-center items-center text-primary-400 mt-2 link-bar" >
                 <a href="mailto:webmasternikos@gmail.com" target="_blank" class="link-item" >
-                    <HomeIcon></HomeIcon>email
+                    <EmailIcon />
                 </a>
 
                 <a href="https://github.com/WebmasterNikos/" target="_blank" class="link-item">
-                    <HomeIcon></HomeIcon>github
+                    <GithubIcon />
                 </a>
 
                 <a href="https://www.linkedin.com/npetalas/" target="_blank" class="link-item">
-                    <HomeIcon></HomeIcon>linkedin
+                    <LinkedinIcon />
                 </a>
 
                 <a href="https://artgen.xyz/" target="_blank" class="link-item">
-                    <HomeIcon></HomeIcon> artgen.xyz
+                    <ArtgenIcon />
                 </a>
             </div>
         </div>
